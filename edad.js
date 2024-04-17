2382423b229a98668439bb6dc7ddadae94e32aac
Luis Mendoza <lmendoza@altimetrik.com>
2024-04-17 10:34:29 -0600

    initial commit

---
+++ edad.js
@@ -0,0 +1,12 @@
+function getEdad(nombre, edad) {
+	if (edad >= 18) {
+		console.log(nombre + ' y Eres mayor de edad');
+	} else {
+		console.log(nombre + ' y Eres menor de edad');
+	}
+}
+
+function repitir(a, b) {
+	return a + b;
+}
+getEdad('Luis', 34);
---
+++ multiplicaciones.js
@@ -0,0 +1,14 @@
+function multiplicar(a,b){
+	return a*b;
+}
+
+function sumar(a,b){
+	return a + b;
+}
+
+function restar(a,b){
+	
+}
+function dividir(a,b){
+	return a/b;
+}
