328b33081ea78df2f81bdbd579c67334f068e37c
Luis Mendoza <lmendoza@altimetrik.com>
2024-04-17 17:03:57 -0600

    updating calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,12 +1,18 @@
-export function sumar(a, b) {
+ function sumar(a, b) {
 	return a + b;
 }
-export function restar(a, b) {
+ function restar(a, b) {
 	return a - b;
 }
-export function multiplicar(a, b) {
+ function multiplicar(a, b) {
 	return a * b;
 }
-export function dividir(a, b) {
+ function dividir(a, b) {
 	return a / b;
+}
+
+
+for (let i = 1; i < 10; i++) {
+	const resultado = multiplicar(1, i);
+	console.log(resultado);
 }
